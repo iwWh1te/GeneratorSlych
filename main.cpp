ee1226d884a211acf1bc50f82d2b8947dc91afc5
iwWh1te <ylanovru10@gmail.com>
2023-05-27 17:19:53 +0400

    chetvertoe

--- main.cpp
+++ main.cpp
@@ -48,11 +48,32 @@ private:
         } else {
             numberLabel->setText(numberLabel->text() + " (нечетное)");
         }
+
+        // Проверка простоты числа и отображение результата
+        if (isPrime(randomNumber)) {
+            numberLabel->setText(numberLabel->text() + " (простое)");
+        } else {
+            numberLabel->setText(numberLabel->text() + " (составное)");
+        }
     }
 
     void resetNumber() {
         randomNumber = 0;
         numberLabel->clear();
+    }
+
+    bool isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+
+        for (int i = 2; i <= number / 2; ++i) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 };
 
